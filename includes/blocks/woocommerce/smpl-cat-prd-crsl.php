1a2dec09da3e5a417d021bdda6dc7b81549e2750
gestok <giorgos.cho@hotmail.com>
2022-03-13 18:18:00 +0200

    WooCommerce Block new options

--- includes/blocks/woocommerce/smpl-cat-prd-crsl.php
+++ includes/blocks/woocommerce/smpl-cat-prd-crsl.php
@@ -18,20 +18,95 @@ if(!empty($block['className'])) $className .= ' ' . $block['className'];
 if(!empty($block['align'])) $className .= ' align' . $block['align'];
 
 
-// Get product count and category slug
+// Get fields
 $count = get_field('prd_count');
+$show_criteria = get_field('show_criteria');
 $cat = get_field('cat_slug');
+$sku = get_field('partial_sku');
+$tag = get_field('tag');
+$weight = get_field('weight');
+$price = get_field('price');
+$t_sales = get_field('total_sales');
+$offset = get_field('offset');
+$order_by = get_field('order_by');
+$order = get_field('order');
+$stock_status = get_field('stock_status');
 
-if (!empty($count) && !empty($cat)){
+/*
+ * Function to determine if all the values are set
+ * @params string
+ * @return boolean
+*/
+function valuesExist($criteria){
+	switch ($criteria) {
+		case 'sku':
+			if (!empty($sku)) return TRUE;
+			break;
+		case 'tag':
+			if (!empty($tag)) return TRUE;
+			break;
+		case 'category':
+			if (!empty($cat)) return TRUE;
+			break;
+		case 'weight':
+			if (!empty($weight)) return TRUE;
+			break;
+		case 'price':
+			if (!empty($price)) return TRUE;
+			break;
+		case 'total_sales':
+			if (!empty($t_sales)) return TRUE;
+			break;
+	}
+	// Invalid or empty info provided
+	return FALSE;
+}
+
+
+if (valuesExist($show_criteria)){
 
     // https://github.com/woocommerce/woocommerce/wiki/wc_get_products-and-WC_Product_Query
     $args = array(
-        'limit'     => intval($count),
-        'category'  => array( strval($cat) ),
-        'orderby'   => 'date',
-        'order'     => 'DESC',
-        'return'    => 'ids',
+        'limit'			=> intval($count),
+		'visibility'	=> 'visible',
+        'return'		=> 'ids',
     );
+
+	switch ($show_criteria) {
+		case 'sku':
+			$args['sku'] = strval($sku);
+			break;
+		case 'tag':
+			$tag = explode(",", strval($tag));
+			$args['tag'] = $tag;
+			break;
+		case 'category':
+			$cat = explode(",", strval($cat));
+			$args['category'] = $cat;
+			break;
+		case 'weight':
+			$args['weight'] = floatval($weight);
+			break;
+		case 'price':
+			$args['price'] = floatval($price);
+			break;
+		case 'total_sales':
+			$args['total_sales'] = intval($t_sales);
+			break;
+	}
+
+	// Extras
+	if (!empty($offset))
+		$args['offset']	= intval($offset);
+	if (!empty($order_by))
+		$args['orderby'] = strval($order_by);
+	if (!empty($order))
+		$args['order'] = strval($order);
+
+	// Stock Status
+	if ($stock_status)
+		$args['stock_status'] = 'instock';
+
     $product_ids = wc_get_products($args);
 
 	// If product IDs are found
