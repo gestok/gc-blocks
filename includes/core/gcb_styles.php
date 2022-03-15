7927b2f3b677e542d11bdc91a2cff1f0598e2a37
George <giorgos.cho@hotmail.com>
2022-03-15 12:13:26 +0200

    Fix URL

--- includes/core/gcb_styles.php
+++ includes/core/gcb_styles.php
@@ -4,6 +4,6 @@
  */
 
 function gcb_additional_css() {
-    wp_enqueue_style('gcb-styles', plugin_dir_path(__FILE__) . 'gcb_styles.css');
+    wp_enqueue_style('gcb-styles', plugin_dir_url(__FILE__) . 'gcb_styles.css');
 }
 add_action('wp_enqueue_scripts', 'gcb_additional_css');
